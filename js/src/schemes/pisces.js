1a36f0319a3ee2bb7e0cc3d4248e8ac31449ddd0
wangliang <wangliang@bboqi.com>
2017-04-18 15:09:26 +0800

    Site updated: 2017-04-18 15:09:25

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -14,9 +14,4 @@ $(document).ready(function () {
       setSidebarMarginTop(getSidebarTop());
     }
   });
-  if($(".instagram").length) {
-        require(['/js/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js'], function(obj) {
-            obj.init();
-        });
-    }
 });
